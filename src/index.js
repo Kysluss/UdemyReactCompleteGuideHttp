f1fa119d37d83102ae6f7bcb76cf1f914cf71624
Kyle Slusser <SLUKY@intlogistics.com>
2018-08-11 08:17:27 -0400

    Added global axios configuration for request/response success and errors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// You can register an interceptor for requests
+// The first argument is a function that can modify the request configuration
+// The second argument is a function for errors when setting up the request
+// For example, if no internet connectivity
+axios.interceptors.request.use(request => {
+    console.log('axios');
+    console.log(request);
+
+    // If you do not return a request object, you will actually block the request
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+// Same thing is true for response objects
+axios.interceptors.response.use(response => {
+    console.log(response);
+    // Edit response
+    return response
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+})
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
