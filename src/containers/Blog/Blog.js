b76983121f23c459803cb4ccea79d504f02638dc
Kyle Slusser <SLUKY@intlogistics.com>
2018-08-11 08:10:15 -0400

    Added error handling for blog retrieval

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,11 +9,12 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [], 
-        selectedPostId: null
+        selectedPostId: null, 
+        error: false
     }
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/postssss')
             .then(response => {
                 //Only showing the first 4 elements
                 const posts = response.data.slice (0, 4);
@@ -29,6 +30,9 @@ class Blog extends Component {
                     posts: updatedPosts
                 });
                 // console.log(response);
+            })
+            .catch(error => {
+                this.setState({error: true});
             });
     }
 
@@ -37,13 +41,16 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)} />
-        });
+        let posts = <p style={{textAlign: 'center'}}>Something went wrong</p>;
+        if(!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
 
         return (
             <div>
