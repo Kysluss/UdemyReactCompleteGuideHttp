0ebeda45f556a5451b91566d300e302172f1a42e
Kyle Slusser <SLUKY@intlogistics.com>
2018-08-11 08:00:45 -0400

    Added individual post rendering

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+    componentDidUpdate() {
+        if(this.props.id) {
+            // If we don't do this check, it will execute an infinite loop because we are calling setState
+            // To get around this, we check if loadedPost hasn't been set yet
+            // If loadedPost does exist already, we need to make sure that the id has changed
+            // If the id hasn't changed, we don't need to execute the http request
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({
+                            loadedPost: response.data
+                        })
+                    });
+            }
+        }
+    }
     render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
-        );
+            // When this.props.id is valid, we execute an asynchronous request
+            // Because of this, we need to check if loadedPost exists
+        if(this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Loading...</p>;
+        }
+        if(this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.content}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+
+            );
+        }
         return post;
     }
 }
