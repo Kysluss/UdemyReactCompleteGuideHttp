36305ccec0f0ad6095d93169b4654e70615c6adc
Kyle Slusser <SLUKY@intlogistics.com>
2018-08-11 08:26:47 -0400

    Created an axios instance

---
+++ src/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+//If you have multiple URLs for your application, you can create separate instances of axios
+// To do this, simply call axios.create
+// It essentially creates a copy of axios or a new instance of axios
+// You can create as many of these as you need
+// The argument is an object of configuration elements
+// This instance will inherit any global configuration changes we made in index.js
+// It will merge the supplied configuration with the global configuration
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+
+export default instance;
